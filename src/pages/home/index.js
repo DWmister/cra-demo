0d5610a5334603b44f6ef3c27504cebcdfe35ec5
zhangsl <zhangsl93@163.com>
2020-12-16 18:26:21 +0800

    整理

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 class Home extends React.Component {
   componentDidMount () {
-    document.title = '剑与远征官方网站-莉莉丝游戏'
+    document.title = 'homehome'
   }
 
   render () {
